4e6636d9db3a4b482643cc0bd26accaa154cc373
Manas <kariamanas7@gmail.com>
2026-01-08 10:16:59 +0000

    update

---
+++ CheckpointWS/src/handshake_pkg/build/handshake_pkg/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/townperson/Documents/TraineeSoftwareModule/CheckpointWS/src/handshake_pkg/install/handshake_pkg/share/handshake_pkg/package.sh"
---
+++ CheckpointWS/src/handshake_pkg/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'handshake_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='MANAS',
+    maintainer_email='kariamanas7@gmail.com',
+    description='Handshake communication between AI and VCU using rclpy',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = handshake_pkg.ai_node:main',
+            'listener = handshake_pkg.vcu_node:main',
+        ],
+    },
+)
